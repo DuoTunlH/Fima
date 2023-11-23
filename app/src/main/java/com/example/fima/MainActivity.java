215076ffe4010a986234e047055037ea591a5f65
DuoTunlH <123788121+DuoTunlH@users.noreply.github.com>
2023-11-23 21:24:13 +0700

    Revert "thong ke"
    This reverts commit ace41eaa1b06e45010ee3b940fb5079ffbf8b18a.

--- app/src/main/java/com/example/fima/MainActivity.java
+++ app/src/main/java/com/example/fima/MainActivity.java
@@ -17,15 +17,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.fima.databinding.ActivityMainBinding;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
 
 public class MainActivity extends AppCompatActivity {
 
