160630d7544b2ca4ca9bf8b4e10edce8b745ccd9
DuoTunlH <123788121+DuoTunlH@users.noreply.github.com>
2023-11-23 22:49:29 +0700

    Update ExpenseViewHolder.java

--- app/src/main/java/com/example/fima/common/ExpenseViewHolder.java
+++ app/src/main/java/com/example/fima/common/ExpenseViewHolder.java
@@ -20,6 +20,8 @@ public class ExpenseViewHolder extends RecyclerView.ViewHolder{
 
     public void setExpense(UserExpense expense) {
         this.expense = expense;
+        name.setText(String.valueOf(expense.getType()));
+        amount.setText(String.valueOf(expense.getAmount()));
     }
 
     public ExpenseViewHolder(@NonNull View itemView) {
