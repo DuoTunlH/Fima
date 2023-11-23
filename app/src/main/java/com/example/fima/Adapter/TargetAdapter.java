19249b0afcdd856451fd6180ad3a302a84dae919
Anhltn1609 <anhsltn1234r@gmail.com>
2023-11-23 21:36:09 +0700

    create ui v1

---
+++ app/src/main/java/com/example/fima/Adapter/TargetAdapter.java
@@ -0,0 +1,74 @@
+package com.example.fima.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.fima.R;
+import com.example.fima.models.Target;
+
+import java.util.ArrayList;
+
+public class TargetAdapter extends RecyclerView.Adapter<TargetAdapter.viewholder> {
+    ArrayList<Target> items;
+    Context context;
+
+    public TargetAdapter(ArrayList<Target> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(context).inflate(R.layout.item_list_target, parent, false);
+        return new viewholder(inflate);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TargetAdapter.viewholder holder, int position) {
+        holder.title.setText(items.get(position).getPlanName());
+
+        int a = (int) (items.get(position).getSavedBudget() / items.get(position).getTotalBudget() * 100);
+        System.out.println(items.get(position).getSavedBudget());
+        holder.processPercent.setText(a + "%");
+        holder.progressBar.setProgress(a);
+        int drawableResourceId = holder.itemView.getResources()
+                .getIdentifier(items.get(position).getImgSrc(), "drawable", holder.itemView.getContext().getPackageName());
+
+        Glide.with(context)
+                .load(drawableResourceId)
+                .into(holder.ic);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        ImageView ic;
+        TextView title, processPercent;
+        ProgressBar progressBar;
+
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+            ic = itemView.findViewById(R.id.icon_target);
+            title = (TextView) itemView.findViewById(R.id.title);
+            processPercent = itemView.findViewById(R.id.progress);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+
+    }
+}
